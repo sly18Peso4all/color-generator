ed7254b2f9f61bce12983292bc561af652a1665c
sly18Peso4all <jomasco18@gmail>
2021-03-08 07:04:08 +0100

    updated project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { useState } from "react";
 import SingleColor from "./SingleColor";
+
 import Values from "values.js";
 
 function App() {
   const [color, setColor] = useState("");
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values("#f15025").all(10));
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+    } catch {
+      setError(true);
+    }
   };
+
   return (
     <>
       <section className="container">
@@ -20,14 +28,24 @@ function App() {
             value={color}
             onChange={(e) => setColor(e.target.value)}
             placeholder="#f15025"
-          ></input>
+            className={`${error ? "error" : null}`}
+          />
           <button className="btn" type="submit">
             submit
           </button>
         </form>
       </section>
       <section className="colors">
-        <SingleColor />
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
       </section>
     </>
   );
